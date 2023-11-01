b048caf87fcd1c2524606546fb90f95ca540cc99
E0717579 <TSuguna@eaton.com>
2023-11-01 11:40:12 +0530

    added  timer trigger

--- TimerTrigger-IB/__init__.py
+++ TimerTrigger-IB/__init__.py
@@ -1,13 +1,13 @@
+import datetime
 import logging
-import json
-import os
+
 import azure.functions as func
-from utils.dcpd.class_installbase import InstallBase
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
 
 
+def main(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+    
     config_dir = "ileads_lead_generation\references"  
     config_file = os.path.join(config_dir, "config_dcpd.json") 
     try:
@@ -19,7 +19,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         # Create an instance of InstallBase and call main_install
         obj = InstallBase(self,conf_env,config)
         result = obj.main_install()
+    if mytimer.past_due:
+        logging.info('The timer is past due!')
 
-        return func.HttpResponse(f"Function result: {result}", mimetype="text/plain")
-    except Exception as e:
-        return func.HttpResponse(f"An error occurred: {str(e)}", status_code=500)
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
