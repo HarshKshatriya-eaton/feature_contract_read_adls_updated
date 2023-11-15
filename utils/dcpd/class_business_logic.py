dd82469606554f3e80f704defb4c72f3a44ce576
E0717579 <TSuguna@eaton.com>
2023-11-15 11:39:44 +0530

    updated business_logic.py

--- utils/dcpd/class_business_logic.py
+++ utils/dcpd/class_business_logic.py
@@ -46,7 +46,7 @@ class BusinessLogic:
                  'adls_dir': self.config['file']['Reference']['decode_sr_num']
                  })
         logging.info(f"Type of ref_prod_fr_srnum: {type(ref_prod_fr_srnum)}")
-        ref_prod_fr_srnum['SerialNumberPattern'] = ref_prod_fr_srnum['SerialNumberPattern'].str.lower()
+       # ref_prod_fr_srnum['SerialNumberPattern'] = ref_prod_fr_srnum['SerialNumberPattern'].str.lower()
         self.ref_prod_fr_srnum = ref_prod_fr_srnum
 
         # Read Reference: Product from TLN
